11bd2c4d79325b603448ab2e15af1e682fa28772
Gabriel Reynoso <garey91@gmail.com>
2025-10-15 15:24:11 +0200

    add get route

--- backend/src/routes/property.route.ts
+++ backend/src/routes/property.route.ts
@@ -1,9 +1,52 @@
 import { Hono } from "hono";
 import { newPropertyValidator } from "../utils/propertyValidator.js";
+import { propertiesQueryValidator } from "../utils/queryPropertiesValidator.js";
 import { requireAuth } from "../middleware/auth.js";
 import { validate as isUUID } from "uuid"
+import type { PostgrestSingleResponse } from "@supabase/supabase-js";
+
 
 const propertyApp = new Hono({ strict: false });
+
+propertyApp.get('/', propertiesQueryValidator, async (c) => {
+    try {
+        const query = c.req.valid("query");
+
+        const offset = query.offset ? query.offset : 0
+        const limit = query.limit ? query.limit : 10
+        const startIndex = offset > 0 ? (offset - 1) : 0
+        const endIndex = startIndex + (limit - 1);
+        const order = query.sort_by ? query.sort_by : "name";
+
+        const sb = c.get("supabase");
+
+        const _query = sb.from("properties").select("*", { count: "exact" }).range(startIndex, endIndex).order(order, { ascending: true })
+
+        if (query.q) {
+            _query.or(`name.ilike.%${query.q}%,description.ilike.%${query.q}%`)
+        }
+
+        const response: PostgrestSingleResponse<Property[]> = await _query;
+        console.log(response);
+
+        if (response.error) {
+            console.error("Error getting properties:", response.error.code, response.error.message)
+            return c.json({ message: "Error getting properties" }, 400)
+        }
+
+        const defaultResponse: PaginatedListResponse<Property> = {
+            data: response.data,
+            offset,
+            limit,
+            count: response.count || 0
+        }
+        console.log(response.data);
+        return c.json({ message: "Success getting properties", properties: defaultResponse }, 200)
+    } catch (error) {
+        console.error(error);
+        return c.json({ message: "Internal server error" }, 500)
+    }
+})
 
 
 propertyApp.post('/', requireAuth, newPropertyValidator, async (c) => {
