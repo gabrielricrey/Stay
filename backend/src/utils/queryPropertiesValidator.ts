aeae90ea84abcbc4f198b0a933ed453791d41a42
Gabriel Reynoso <garey91@gmail.com>
2025-10-15 15:23:42 +0200

    add queryvalidator

---
+++ backend/src/utils/queryPropertiesValidator.ts
@@ -0,0 +1,11 @@
+import * as z from "zod";
+import { zValidator } from "@hono/zod-validator";
+
+const schema: z.ZodType<PropertiesQuery> = z.object({
+    offset: z.coerce.number().optional(),
+    limit: z.coerce.number().optional(),
+    sort_by: z.enum(["name", "created_at", "price_per_night"]).optional(),
+
+})
+
+export const propertiesQueryValidator = zValidator("query", schema);
