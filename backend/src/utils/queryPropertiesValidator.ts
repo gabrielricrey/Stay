9e654477aa5d1b3f2a12df5fd5abf059d8604197
Gabriel Reynoso <garey91@gmail.com>
2025-10-15 15:49:19 +0200

    add optional on q in zod schema

--- backend/src/utils/queryPropertiesValidator.ts
+++ backend/src/utils/queryPropertiesValidator.ts
@@ -4,7 +4,7 @@ import { zValidator } from "@hono/zod-validator";
 const schema: z.ZodType<PropertiesQuery> = z.object({
     offset: z.coerce.number().optional(),
     limit: z.coerce.number().optional(),
-    q: z.string(),
+    q: z.string().optional(),
     sort_by: z.enum(["name", "created_at", "price_per_night"]).optional(),
 
 })
