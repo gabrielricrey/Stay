659754bcf94891fbed24b3f4e2cbecab76aa7ee3
Gabriel Reynoso <garey91@gmail.com>
2025-10-15 12:42:50 +0200

    add property routes to app

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 import authApp from './routes/auth.route.js'
+import propertyApp from './routes/property.route.js'
 import { withSupabase, requireAuth } from './middleware/auth.js'
 
 const app = new Hono()
@@ -8,6 +9,7 @@ const app = new Hono()
 app.use("*", withSupabase);
 
 app.route('/auth', authApp);
+app.route('/property', propertyApp);
 
 serve({
   fetch: app.fetch,
