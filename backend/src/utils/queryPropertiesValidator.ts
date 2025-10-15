436d42161efdd68e54456c5754b519c2b702575d
Gabriel Reynoso <garey91@gmail.com>
2025-10-15 15:29:03 +0200

    add q to queryValidator

--- backend/src/utils/queryPropertiesValidator.ts
+++ backend/src/utils/queryPropertiesValidator.ts
@@ -4,6 +4,7 @@ import { zValidator } from "@hono/zod-validator";
 const schema: z.ZodType<PropertiesQuery> = z.object({
     offset: z.coerce.number().optional(),
     limit: z.coerce.number().optional(),
+    q: z.string(),
     sort_by: z.enum(["name", "created_at", "price_per_night"]).optional(),
 
 })
