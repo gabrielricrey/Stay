b449ddc255a082f977ec2ea79614bb15c3e31646
Gabriel Reynoso <garey91@gmail.com>
2025-10-16 22:16:52 +0200

    create booking types

---
+++ backend/src/types/booking.d.ts
@@ -0,0 +1,16 @@
+interface NewBooking {
+    property_id: string;
+    user_id?: string;
+    check_in_date: string;
+    check_out_date: string;
+    total_cost?: number;
+}
+
+interface Booking extends NewBooking {
+    id: string;
+    created_at: string;
+    updated_at: string;
+    status: "pending" | "confirmed" | "cancelled";
+
+}
+
