e6f1b3ba71650e71471c949c149877ea869df277
Gabriel Reynoso <garey91@gmail.com>
2025-10-17 20:40:30 +0200

    new schema and validator for editing booking

--- backend/src/utils/bookingValidator.ts
+++ backend/src/utils/bookingValidator.ts
@@ -1,10 +1,18 @@
 import * as z from "zod";
 import { zValidator } from "@hono/zod-validator";
 
-const schema: z.ZodType<NewBooking> = z.object({
+const newBookingschema: z.ZodType<NewBooking> = z.object({
     property_id: z.string(),
     check_in_date: z.string(),
-    check_out_date: z.string()
-})
+    check_out_date: z.string(),
+});
 
-export const newBookingValidator = zValidator("json", schema)
+const editBookingschema: z.ZodType<Partial<Booking>> = z.object({
+    property_id: z.string(),
+    check_in_date: z.string().optional(),
+    check_out_date: z.string().optional(),
+    status: z.enum(["pending", "cancelled", "confirmed", "completed"]).optional()
+});
+
+export const newBookingValidator = zValidator("json", newBookingschema);
+export const editBookingValidator = zValidator("json", editBookingschema);
