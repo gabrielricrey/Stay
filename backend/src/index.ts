7265c646a490eca7c9e63e1a3c1c55a3227f7efb
Gabriel Reynoso <garey91@gmail.com>
2025-11-03 13:12:53 +0100

    fix: update CORS configuration to allow requests from localhost:3001

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,12 +12,14 @@ import { withSupabase, requireAuth } from './middleware/auth.js'
 
 const app = new Hono({ strict: false })
 
+
 app.use('*', cors({
-  origin: 'http://localhost:3000',
-  allowHeaders: ['Authorization', 'Content-Type'],
+  origin: ['http://localhost:3001/', 'https://stay-two.vercel.app'],
+  allowHeaders: ['Authorization', 'Content-Type', 'Access-Control-Allow-Origin'],
   allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true
 }))
+
 app.use("*", withSupabase);
 
 app.route('/auth', authApp);
